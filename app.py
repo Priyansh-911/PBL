1d4e254af81b2871451f3c7a44143edc24ae76c7
SAMAKSH10 <108063107+SAMAKSH10@users.noreply.github.com>
2023-05-26 09:15:55 -1200

    backend update

--- app.py
+++ app.py
@@ -1,19 +1,50 @@
 from flask import Flask, render_template, session, request, redirect
+import pyrebase
 
 app = Flask(__name__)
+config = {
+    'apiKey': "AIzaSyD-HUi7t4AxKJCjdZoJwXNh1QjGL4Emfx8",
+    'authDomain': "my-first-project-903ae.firebaseapp.com",
+    'projectId': "my-first-project-903ae",
+    'storageBucket': "my-first-project-903ae.appspot.com",
+    'messagingSenderId': "469417932442",
+    'appId': "1:469417932442:web:8fd49fe77cbd1b0ece9123",
+    'databaseURL' : ""
+}
+firebase = pyrebase.initialize_app(config)
+auth=firebase.auth()
+app.secret_key='secret'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/authenticate')
+def authenticate():
+    if request.method == 'POST':
+        email=request.form.get('email')
+        password=request.form.get('passowrd')
+        try:
+            user = auth.sign_in_with_email_and_password(email,password)
+            session['user']=email
+        except:
+            return 'Failed to login'
+    if user in session:
+        return 'Hello User '
+    
 
-@app.route('/register')
+@app.route('/login',methods=['POST','GET'])
+def login():
+     return render_template('login.html')
+    
+@app.route('/register',methods=['POST','GET'])
 def register():
+
     return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=False)
--- auth.py
+++ auth.py
@@ -1,14 +1,18 @@
 import pyrebase 
 
 config = {
-    'apiKey' : "AIzaSyCb4i2IwnW4-Bh3QQA0O5OOiRg5I4oyv4o",
-    'authDomain' : "aktu-auth.firebaseapp.com",
-    'projectId' : "aktu-auth",
-    'storageBucket' : "aktu-auth.appspot.com",
-    'messagingSenderId' : "876112490224",
-    'appId' : "1:876112490224:web:21be6c4fe345070a195c35",
-    'measurementId' : "G-KBKBC66YVP",
+    'apiKey': "AIzaSyD-HUi7t4AxKJCjdZoJwXNh1QjGL4Emfx8",
+    'authDomain': "my-first-project-903ae.firebaseapp.com",
+    'projectId': "my-first-project-903ae",
+    'storageBucket': "my-first-project-903ae.appspot.com",
+    'messagingSenderId': "469417932442",
+    'appId': "1:469417932442:web:8fd49fe77cbd1b0ece9123",
     'databaseURL' : ""
 }
 
 firebase = pyrebase.initialize_app(config)
+auth=firebase.auth()
+email="testing@gmail.com"
+password="123456"
+user=auth.create_user_with_email_and_password(email,password)
+print(user)
